eaa6e8bc778d8d7b4a8abd4a0721ab20ec537fb9
DarkHagen <nwcdragon.h@gmail.com>
2014-07-25 02:43:13 +0300

    beginning vsprintf

--- kernel/vsprintf.c
+++ kernel/vsprintf.c
@@ -1,9 +1,62 @@
 /* File: vsprintf.c
  * Author: Aleksei Karpenko <nwcdragon.h@gmail.com>
  */
-/*
+
 #include <stdarg.h>
-#include <x86os/types.h>
-#include <x86os/asm.h>
 #include <string.h>
-*/
+
+#define PLUS  1
+#define LEFT  2
+#define ZERO  4
+#define SMALL 8
+#define SIGN  16
+#define SPACE 32
+
+int vsprintf(char *buf, const char *fmt, va_list args)
+{
+	char* element;
+	int flags;
+	for( element = buf; *fmt; fmt++ )
+	{
+		if ( *fmt != "%" )
+		{
+			*element++ = *fmt;
+			continue;
+		}
+		
+		flags = 0;
+		rep:
+			++fmt;
+			switch (*fmt){
+				case '+': flags |= PLUS; goto rep;
+				case '-': flags |= LEFT; goto rep;
+				case '0': flags |= ZERO; goto rep;
+			}
+		// size?
+		switch ( *fmt ){
+			case 'x':
+		// add all specifiers
+		}
+
+	}
+	
+	
+}
+
+static char* num(char* str, int num, int base, int type)	// parameter (int size) if need size 
+{
+	char c, sign, tmp[16];
+	const char *digits="0123456789ABCDEF";
+	int i;
+	
+	c = (type&ZEROPAD) ? '0' : ' ' ;
+	if (type&SMALL) digits="0123456789abcdef";
+	if (base<2 || base>16)
+		return 0;
+	if (type&SIGN && num<0) {
+		sign='-';
+		num = -num;
+	} else
+		sign=(type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
+	//the other situation
+}
